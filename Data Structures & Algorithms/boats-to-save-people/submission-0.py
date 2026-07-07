dd1f2b3132b3f7adeca144a31c771e2525d1e320
Nishanth Reddy Basava <nishanthrbasava@gmail.com>
2026-07-07 13:44:07 -0400

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        # Time Complexity: O(nlogn)
+        # Space Complexity: O(n)
+
+        people.sort()
+
+        p1 = 0 
+        p2 = len(people) - 1
+        count = 0
+
+        while p1 <= p2:
+            if people[p1] + people[p2] > limit:
+                count += 1
+                p2 -= 1
+            else:
+                count += 1
+                p1 += 1
+                p2 -= 1
+
+        return count
