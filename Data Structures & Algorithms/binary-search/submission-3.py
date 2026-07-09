c9c34b0257e0d2aea82d02a73090e7ce26dc4b6b
Nishanth Reddy Basava <nishanthrbasava@gmail.com>
2026-07-09 14:45:42 -0500

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # Time Complexity: O(logn)
+        # Space Complexity: O(1)
+
+        p1 = 0
+        p2 = len(nums) - 1
+
+        while p1 < p2:
+            mid = (p1 + p2) // 2
+
+            if nums[mid] >= target:
+                if nums[mid] == target:
+                    return mid
+
+                p2 = mid
+            else: 
+                p1 = mid + 1
+
+        if p1 == len(nums) or nums[p1] != target:
+            return -1
+
+        return p1
