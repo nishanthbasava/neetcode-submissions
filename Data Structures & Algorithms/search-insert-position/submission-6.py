91dc6230fc8470ae7825874ce7dd735507a25333
Nishanth Reddy Basava <nishanthrbasava@gmail.com>
2026-07-09 13:47:54 -0500

    Add: search-insert-position - submission-6

---
+++ Data Structures & Algorithms/search-insert-position/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+        p1 = 0 
+        p2 = len(nums) - 1
+
+        while p1 <= p2:
+            p3 = (p1 + p2) // 2
+
+            if nums[p3] < target:
+                p1 = p3 + 1
+            elif nums[p3] > target:
+                p2 = p3 - 1
+            else:
+                return p3 
+
+        return p1
