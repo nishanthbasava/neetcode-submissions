1bc741db5563a827087f1a30fcaf6dbc0f37bdec
Nishanth Reddy Basava <nishanthrbasava@gmail.com>
2026-07-15 16:04:50 -0500

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,45 @@
+class TrieNode: 
+    def __init__(self, value = "", flag = False):
+        self.pointers = {}
+        self.flag = flag #marks end of each word
+        self.value = value
+
+class PrefixTree:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        for i, char in enumerate(word):
+            if char not in cur.pointers:
+                cur.pointers[char] = TrieNode(value = char)
+
+            cur = cur.pointers[char]
+
+            if i == len(word) - 1:
+                cur.flag = True
+
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+
+        for char in word:
+            if char not in cur.pointers:
+                return False
+        
+            cur = cur.pointers[char]
+
+        return cur.flag
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+
+        for char in prefix:
+            if char not in cur.pointers:
+                return False
+        
+            cur = cur.pointers[char]
+
+        return True
